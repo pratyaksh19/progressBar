2eb44a3fb41602e23086f42a44451c603b98ee01
Pratyaksh Prasoon <prapraso@cisco.com>
2022-03-04 21:49:03 +0530

    first commit

---
+++ progressbar.js
@@ -0,0 +1,6 @@
+// add logic so that animation should work only when we clicks button
+
+function upload() {
+    const progressbar=document.querySelector('.progress-bar');
+    progressbar.setAttribute('id','play-animation');
+}
